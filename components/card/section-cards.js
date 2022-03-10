db9ab82057cb2acddc0930dfe906e52f724d916c
godwin <smarttin77@gmail.com>
2022-03-10 23:16:00 +0100

    home and video page

---
+++ components/card/section-cards.js
@@ -0,0 +1,29 @@
+import Card from "./card";
+import Link from "next/link";
+import clsx from "classnames";
+import styles from "./section-cards.module.css";
+
+const SectionCards = (props) => {
+  const { title, videos = [], size, shouldWrap = false, shouldScale } = props;
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={clsx(styles.cardWrapper, shouldWrap && styles.wrap)}>
+        {videos.map((video, idx) => (
+          <Link href={`/video/${video.id}`} key={idx}>
+            <a>
+              <Card
+                id={idx}
+                imgUrl={video.imgUrl}
+                size={size}
+                shouldScale={shouldScale}
+              />
+            </a>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default SectionCards;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,31 @@
 import Head from "next/head";
-import Image from "next/image";
+import Banner from "../components/banner/banner";
+import NavBar from "../components/nav/navbar";
+import { getPopularVideos, getVideos } from "../lib/videos";
 import styles from "../styles/Home.module.css";
+import SectionCards from "../components/card/section-cards";
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  const disneyVideos = await getVideos("disney trailer");
+  const productivityVideos = await getVideos("Productivity");
+  const travelVideos = await getVideos("indie music");
+  const popularVideos = await getPopularVideos();
+  return {
+    props: {
+      disneyVideos,
+      travelVideos,
+      productivityVideos,
+      popularVideos,
+    },
+  };
+}
+
+export default function Home({
+  disneyVideos,
+  travelVideos,
+  productivityVideos,
+  popularVideos,
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -10,7 +33,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>Nextflix</main>
+      <main className={styles.main}>
+        <NavBar username="ankita@ank.com" />
+        <Banner
+          videoId="4zH5iYM4wJo"
+          title="Clifford the red dog"
+          subTitle="a very cute dog"
+          imgUrl="/static/clifford.webp"
+        />
+
+        <div className={styles.sectionWrapper}>
+          <SectionCards title="Disney" videos={disneyVideos} size="large" />
+          <SectionCards title="Travel" videos={travelVideos} size="small" />
+          <SectionCards title="Productivity" videos={productivityVideos} size="medium" />
+          <SectionCards title="Popular" videos={popularVideos} size="small" />
+        </div>
+      </main>
     </div>
   );
 }
